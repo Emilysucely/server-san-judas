68ae996e061812cb959fed58d1d126b4e33d0c8c
Emily <emilyrecinosargujo@sanjudastadeo.edu.gt>
2025-10-09 16:05:59 -0600

    9/10/2025

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,13 +1,25 @@
-import { Router } from "express";
+import { Router } from "express"
 import { register, login } from './auth.controller.js'
-import { uploadProfilePicture } from "../../middlewares/file-uploader.js"
+import { uploadProfilePicture } from "../../middlewares/file-uploader.js";
+import { loginValidator, registerValidator } from "../../middlewares/auth-validator.js";
 
 const router = Router()
 
-router.post('/register', uploadProfilePicture.single('profilePicture'), register)
+router.post('/register', 
+    uploadProfilePicture.single('profilePicture'), 
+    registerValidator,
+    register
+)
 
 router.post('/register', register)
 
-router.post('/login', login)
+router.post('/login', loginValidator, login)
 
-export default router 
+export default router   
+
+ 
+ 
+ 
+                     
+
+
